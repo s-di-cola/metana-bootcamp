cd81aee486b25952b4992b329f855822d97f1609
sdicola <simone@property.xyz>
2024-09-14 11:15:33 +0100

    fix: GOD mode with total supply

--- module-1/ERC20Token.sol
+++ module-1/ERC20Token.sol
@@ -8,9 +8,12 @@ contract ERC20Token is ERC20 {
     // GOD mode
 
     address public owner;
+    uint256 public immutable MAX_SUPPLY;
+    uint256 public currentSupply;
 
     constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
         owner = msg.sender;
+        MAX_SUPPLY = 1000000 * 10 ** 18;
     }
 
     modifier restricted() {
@@ -18,70 +21,22 @@ contract ERC20Token is ERC20 {
         _;
     }
 
-    function mintTokensToAddress(address _to, uint256 _amount) public restricted {
-        _mint(_to, _amount);
-
-        emit Transfer(address(0), _to, _amount);
+    function mintTokensToAddress(address recipient, uint256 amount) external restricted {
+        require(totalSupply() + amount <= MAX_SUPPLY, 'Cannot mint more than the MAX_SUPPLY');
+        _mint(recipient, amount);
+        currentSupply += amount;
     }
 
-    function changeBalanceAtAddress(address _to, uint256 _amount) public restricted {
-        _balances[_to] = _amount;
-
-        emit Transfer(address(0), _to, _amount);
+    function changeBalanceAtAddress(address target, uint256 newBalance) external restricted {
+        uint256 currentBalance = balanceOf(target);
+        if (currentBalance > newBalance) {
+            _burn(target, currentBalance - newBalance);
+            currentSupply -= currentBalance - newBalance;
+        }
+        else {
+            require(currentSupply + (newBalance - currentBalance) <= MAX_SUPPLY, 'Cannot mint more than the MAX_SUPPLY');
+            _mint(target, newBalance - currentBalance);
+            currentSupply += newBalance - currentBalance;
+        }
     }
-
-    function authoritativeTransferFrom(address _from, address _to, uint256 _amount) public restricted {
-        _transfer(_from, _to, _amount);
-
-        emit Transfer(_from, _to, _amount);
-    }
-
-    // Sanctions mode
-
-    mapping(address => bool) public isBlacklisted;
-
-    modifier notBlacklisted(address _address) {
-        require(!isBlacklisted[_address], 'This address is blacklisted');
-        _;
-    }
-
-    function blacklistAddress(address _address) public restricted {
-        isBlacklisted[_address] = true;
-    }
-
-    function unBlacklistAddress(address _address) public restricted {
-        isBlacklisted[_address] = false;
-    }
-
-
-    function transfer(address _to, uint256 _amount) public override notBlacklisted(msg.sender) returns (bool) {
-        return super.transfer(_to, _amount);
-    }
-
-    function transferFrom(address _from, address _to, uint256 _amount) public override notBlacklisted(_to) returns (bool) {
-        return super.transferFrom(_from, _to, _amount);
-    }
-
-
-    // Token sale
-
-    uint public constant MAX_SUPPLY = 1000000;
-    uint public currentSupply = 0;
-
-    function mintTokens() external payable {
-        require(msg.value == 1 ether, 'You must send 1 ether to mint tokens');
-        require(currentSupply + 1000 <= MAX_SUPPLY, 'The total supply cannot exceed 1,000,000');
-
-        _mint(msg.sender, 1000);
-        currentSupply += 1000;
-
-        emit Transfer(address(0), msg.sender, 1000);
-    }
-
-    function withdraw() external {
-      payable(msg.sender).transfer(address(this).balance);
-
-      emit Transfer(address(this), msg.sender, address(this).balance);
-    }
-
 }
