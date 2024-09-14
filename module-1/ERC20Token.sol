4949136249bf4ab99ba676737b1802a90a0f7f1b
sdicola <simone@property.xyz>
2024-09-14 09:59:59 +0100

    feat: adds god mode

--- module-1/ERC20Token.sol
+++ module-1/ERC20Token.sol
@@ -4,4 +4,29 @@ pragma solidity ^0.8.20;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Token is ERC20 {
+
+    // GOD mode
+
+    address public owner;
+
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        owner = msg.sender;
+    }
+
+    modifier restricted() {
+        require(msg.sender == owner, 'Only the contract owner can call this function');
+        _;
+    }
+
+    function mintTokensToAddress(address _to, uint256 _amount) public restricted {
+        _mint(_to, _amount);
+    }
+
+    function changeBalanceAtAddress(address _to, uint256 _amount) public restricted {
+        _balances[_to] = _amount;
+    }
+
+    function authoritativeTransferFrom(address _from, address _to, uint256 _amount) public restricted {
+         _transfer(_from, _to, _amount);
+    }
 }
