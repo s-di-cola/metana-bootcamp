9f548662f82c110609d4a7964c1911bb2367f741
sdicola <simone@property.xyz>
2024-09-20 16:37:08 +0100

    chore: refactor minter constructor

--- module-2/deliverables/2_payable_nft/ERC721Exchange.sol
+++ module-2/deliverables/2_payable_nft/ERC721Exchange.sol
@@ -4,22 +4,35 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract ERC721Exchange is ERC721 {
-
     uint256 private _tokenIdCounter;
-    address private immutable minter;
+    address public minter;
+    address public owner;
 
-    constructor(address _minter)ERC721("ERC721Exchange", "ERC721X"){
-        minter = _minter;
+    constructor() ERC721("ERC721Exchange", "ERC721X") {
+        owner = msg.sender;
     }
 
-    modifier onlyMinter(){
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    modifier onlyMinter() {
         require(msg.sender == minter, "Only the minter can call this function");
         _;
     }
 
-    function mint(address _to) external onlyMinter returns (uint256){
+    function setMinter(address _minter) external onlyOwner {
+        require(_minter != address(0), "Minter address cannot be zero");
+        minter = _minter;
+    }
+
+    function mint(address _to) external onlyMinter returns (uint256) {
         _safeMint(_to, _tokenIdCounter++);
         return _tokenIdCounter;
     }
 
+    function getCurrentTokenId() public view returns (uint256) {
+        return _tokenIdCounter;
+    }
 }
--- module-2/deliverables/2_payable_nft/Minter.sol
+++ module-2/deliverables/2_payable_nft/Minter.sol
@@ -6,41 +6,33 @@ import "./ERC721Exchange.sol";
 import {ERC20Exchange} from "./ERC20Exchange.sol";
 
 contract Minter {
-
-    // Set the price of the NFT to 10 TEX tokens
     uint256 public constant NFT_PRICE = 10 * 1e18;
-    ERC20Exchange private erc20Exchange;
-    ERC721Exchange private erc721Exchange;
-    address private owner;
+    ERC20Exchange public immutable erc20Exchange;
+    ERC721Exchange public immutable erc721Exchange;
+    address public owner;
 
-    constructor(ERC20Exchange _erc20Exchange) {
-        erc20Exchange = _erc20Exchange;
-        erc721Exchange = new ERC721Exchange(address(this));
+    constructor(address _erc20Exchange, address _erc721Exchange) {
+        require(_erc20Exchange != address(0), "ERC20Exchange address cannot be zero");
+        require(_erc721Exchange != address(0), "ERC721Exchange address cannot be zero");
+        erc20Exchange = ERC20Exchange(_erc20Exchange);
+        erc721Exchange = ERC721Exchange(_erc721Exchange);
         owner = msg.sender;
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Unauthorized");
+        require(msg.sender == owner, "Only the owner can call this function");
         _;
     }
 
     function mint() external {
-        //Check if the user has enough TEX tokens to purchase the NFT
         require(erc20Exchange.balanceOf(msg.sender) >= NFT_PRICE, "Insufficient balance");
-
-        // Check if the user has approved the contract to spend TEX tokens
         require(erc20Exchange.allowance(msg.sender, address(this)) >= NFT_PRICE, "Insufficient allowance");
-
-        // Transfer TEX tokens from the user to the contract
         require(erc20Exchange.transferFrom(msg.sender, address(this), NFT_PRICE), "Transfer failed");
-
-        // Mint the NFT and transfer it to the user
         erc721Exchange.mint(msg.sender);
     }
 
-
-    function withdraw(uint256 _amount) external onlyOwner{
-        require(address (this).balance >= _amount, "Insufficient balance");
-        require(erc20Exchange.transfer(owner, _amount), "Transfer failed");
+    function withdrawTEX(uint256 amount) external onlyOwner {
+        require(erc20Exchange.balanceOf(address(this)) >= amount, "Insufficient TEX balance");
+        require(erc20Exchange.transfer(owner, amount), "Transfer failed");
     }
 }
