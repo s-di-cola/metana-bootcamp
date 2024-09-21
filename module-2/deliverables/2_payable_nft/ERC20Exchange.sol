d5e074f5b359d3312683e6b9d99384d3abf9bec6
sdicola <simone@property.xyz>
2024-09-21 09:09:45 +0100

    chore: renames files

--- module-2/deliverables/2_payable_nft/ERC20Exchange.sol
+++ module-2/deliverables/2_payable_nft/ERC20Exchange.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import {ERC20Token} from "../../../module-1/ERC20Token.sol";
+import {ERC20Token} from "../../../module-1/deliverables/1_ERC20Token.sol";
 
 contract ERC20Exchange is ERC20Token{
     constructor() ERC20Token("TokenExchange", "TEX"){}
